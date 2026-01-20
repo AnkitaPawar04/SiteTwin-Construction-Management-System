b99b88ddab6ea2d475c0f648f0f748af711dd8b6
atharvagitaye <atharvagitaye@gmail.com>
2026-01-20 22:51:54 +0530

    Minor fixes

--- mobile/lib/core/constants/api_constants.dart
+++ mobile/lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   // Base URL - Update this to your backend URL
-  static const String baseUrl = 'http://10.0.2.2:8000/api'; // Android emulator
+  static const String baseUrl = 'http://192.168.1.2:8000/api'; // Android emulator
   // static const String baseUrl = 'http://localhost:8000/api'; // iOS simulator
   // static const String baseUrl = 'https://your-domain.com/api'; // Production
   
